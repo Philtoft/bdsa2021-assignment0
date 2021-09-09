c637b1501c41fb930d74fdbf25f7b9bc27df4d3a
Philip August Hyltoft <philip.hyltoft@gmail.com>
2021-09-09 17:00:35 +0200

    Is leap year (#3)
    * Some stuff changed
    
    * desc cleanup
    
    * is leap year tests add
    
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear
    
    * Implemented error handling
    
    * Code documented
    
    * Added documentation
    
    * word- & pdf documents not tracked

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,49 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Enter a year");
+                string yearInput = Console.ReadLine();
+
+                int year = Int32.Parse(yearInput);
+
+                if (isLeapYear(year))
+                {
+                    Console.WriteLine("yay");
+                }
+                else
+                {
+                    Console.WriteLine("nay");
+                }
+            }
+            catch (SystemException)
+            {
+                throw new Exception("A number was excepted, but another data type was received");
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Something went wrong", e);
+            }
+        }
+
+        public static bool isLeapYear(int year)
+        {
+            // Check if it is a complete century
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            if (year % 4 == 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public static bool isLeapYear(int year)
