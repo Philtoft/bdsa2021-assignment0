24e1906444fef7387a2aab878f6b28c2501860df
Philip August Hyltoft <philip.hyltoft@gmail.com>
2021-09-09 15:47:07 +0200

    Is leap year (#2)
    * Some stuff changed
    
    * desc cleanup
    
    * is leap year tests add

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,5 +8,24 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public static bool isLeapYear(int year)
+        {
+            // Check if it is a complete century
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            if (year % 4 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
